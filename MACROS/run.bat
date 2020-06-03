ccdb344d14a7dde81d09b618215ca4732d5564b5
Huy Nguyen <vhuy.nguyen@sift-ag.com>
2020-06-03 16:57:28 +0700

    upload resources

---
+++ MACROS/run.bat
@@ -0,0 +1,1 @@
+..\doctotext\doctotext.exe --pdf D:\workshop_june_2020\data_input\guam\2019\november-2019-preliminary-arrival-summary.pdf > output.txt
---
+++ run.bat
@@ -0,0 +1,2 @@
+doctotext\doctotext.exe --pdf D:\alteryx-pdf\read-pdf-on-Alteryx\2020\february-2020-preliminary-arrival-summary.pdf >> output.txt
+doctotext\doctotext.exe --pdf D:\alteryx-pdf\read-pdf-on-Alteryx\2020\january-2020-preliminary-arrival-summary.pdf >> output.txt
