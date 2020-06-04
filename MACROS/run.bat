2a3e8a39ca934e529f632cc2effe8de3da538fb5
Huy Nguyen <huynv2909@gmail.com>
2020-06-04 11:26:17 +0700

    update on new machine

--- MACROS/run.bat
+++ MACROS/run.bat
@@ -1,1 +1,1 @@
-..\doctotext\doctotext.exe --pdf D:\workshop_june_2020\data_input\guam\2019\november-2019-preliminary-arrival-summary.pdf > output.txt
+..\doctotext\doctotext.exe --pdf D:\tourism_prep\data_input\guam\2019\november-2019-preliminary-arrival-summary.pdf > output.txt
